85bb6e121ed1ceecf535c436baeaea2595678586
lumanskiy <lumansk@uchicago.edu>
2018-04-12 23:46:08 -0500

    New stuff

--- Leah-Vaccine.R
+++ Leah-Vaccine.R
@@ -1,6 +1,6 @@
 library(readr)
-myData <- read.csv("~/Desktop/health_cohort_data.csv", header = T)
-View(myData)
+df <- read.csv("~/Desktop/health_cohort_data.csv", header = T)
+View(df)
 
 library(dplyr)
 library(ggplot2)
@@ -9,8 +9,13 @@ library(knitr)
 library(purrr)
 library(tidyr)
 library(stringr)
-myData$VACFLU12M
+df$VACFLU12M
 
 vaccination_codebook <-
   tibble(VACFLU12M = c(0,1,2,3,7,8,9),
          vaccination_clean = c("NIU","No", "Yes", "Reported both shot and spray", "Refused", "Not Ascertained", "Don't Know"))
+
+df_clean <-
+  df %>%
+  left_join(vaccination_codebook, by = "VACFLU12M")
+df_clean
