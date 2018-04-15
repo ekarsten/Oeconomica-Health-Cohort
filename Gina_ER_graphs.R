0dd1e2d5525e2652c89b6050313d1c1328f0d140
ginakim1st <ginakim1st@uchicago.edu>
2018-04-15 13:21:14 -0500

    graphs

---
+++ Gina_ER_graphs.R
@@ -0,0 +1,18 @@
+
+root <- getwd()
+while(basename(root) != "Oeconomica-Health-Cohort") {
+  root <- dirname(root)
+}
+
+source(file.path(root, "data.R"))
+
+library(dplyr)
+library(ggplot2)
+library(lubridate)
+library(knitr)
+library(purrr)
+library(tidyr)
+library(stringr)
+
+load(file.path(ddir, "clean_health_cohort_data.rda"))
+
