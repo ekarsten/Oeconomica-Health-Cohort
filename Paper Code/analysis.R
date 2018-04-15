3df397eb126b26b35edd6fb5001b6be8e2eab111
EricKarsten <eric.t.karsten@gmail.com>
2018-04-15 13:58:03 -0500

    vaccination analysis code

--- Paper Code/analysis.R
+++ Paper Code/analysis.R
@@ -1,5 +1,5 @@
 #====================================
-# Data Cleaning
+# Data Analysis
 #====================================
 
 #-----------------------------------
@@ -14,6 +14,29 @@ while(basename(root) != "Oeconomica-Health-Cohort") {
 source(file.path(root, "data.R"))
 
 library(dplyr)
+library(ggplot2)
 
 load(file.path(ddir, "clean_health_cohort_data.Rda"))
 
+#-----------------------------------
+# Making A figure for proportion vaccinated over time
+#-----------------------------------
+
+df %>%
+  group_by(YEAR) %>%
+  summarise(prop_vaccinated = sum(vaccination == "Yes", na.rm = T )/n()  ) %>%
+  ggplot(aes(x = as.character(YEAR), y = prop_vaccinated)) +
+  geom_point() +
+  theme_bw() +
+  labs(x = "Year",
+       y = "Proportion Vaccinated")
+
+df %>%
+  group_by(YEAR, sex, race) %>%
+  summarise(prop_vaccinated = sum(vaccination == "Yes", na.rm = T )/n()  ) %>%
+  ggplot(aes(x = as.character(YEAR), y = prop_vaccinated, color = sex)) +
+  geom_point() +
+  theme_bw() +
+  facet_wrap(~race) +
+  labs(x = "Year",
+       y = "Proportion Vaccinated")
