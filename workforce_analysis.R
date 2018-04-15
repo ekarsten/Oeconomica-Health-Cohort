3a93edc14496db5e921a2bd99b87fa30225c1a53
devinmcnulty <mcnultydp@gmail.com>
2018-04-15 13:57:17 -0500

    no hc bc aged out

--- workforce_analysis.R
+++ workforce_analysis.R
@@ -20,8 +20,22 @@ load(file.path(ddir, "clean_health_cohort_data.rda"))
 
 u26_df <-
   df %>%
-  filter(age <= 26, age >= 18, hourswrk != NA)
+  filter(age <= 26, age >= 18, !is.na(empstat)) %>%
+  group_by(YEAR) %>%
+  summarise(emprate = sum(empstat == "Employed")/sum(age <= 26))
   
 u26_df %>%
-  ggplot(aes(x = YEAR, y = hourswrk)) +
+  ggplot(aes(x = YEAR, y = emprate)) +
   geom_point() 
+
+u26_df <-
+  df %>%
+  filter(age <= 26, age >= 18, !is.na(hinoager)) %>%
+  group_by(YEAR) %>%
+  summarise(n_yes = sum(hinoager == "Yes")/sum(age <= 26))
+
+u26_df %>%
+  ggplot(aes(x = YEAR, y = n_yes)) +
+  geom_point() 
+
+
