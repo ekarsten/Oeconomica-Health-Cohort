258f7b1f04317b3a83813044cf8aa394505922a4
ginakim1st <ginakim1st@uchicago.edu>
2018-04-15 13:56:58 -0500

    graphs

--- Gina_ER_graphs.R
+++ Gina_ER_graphs.R
@@ -14,5 +14,25 @@ library(purrr)
 library(tidyr)
 library(stringr)
 
+
 load(file.path(ddir, "clean_health_cohort_data.rda"))
+
+
+countER_df <-
+  df %>%
+  group_by(YEAR) %>%
+  summarize(N_Yes = sum(delaycost == "Yes",na.rm=TRUE),
+            N_No = sum(delaycost == "No",na.rm=TRUE),
+            N_Total = n()) %>% 
+  mutate(prop_yes=N_Yes/(N_Yes+N_No))
+
+g1<-countER_df %>%
+  ggplot(aes(x=YEAR, y=prop_yes)) +
+  geom_point()
+g1
+
+g2<-countER_df %>%
+  ggplot(aes(x=YEAR, y=N_yes)) +
+  geom_point()
+g2
 
